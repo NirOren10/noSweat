fd6d1aa4ffe6134065466730fdef654777ec6021
giladbeja <giladbeja@yahoo.com>
2024-03-23 17:26:10 -0400

    sdf

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import sqlite3
 import json
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_session import Session
 from tempfile import mkdtemp
 from helper import login_required
